d0e40ab9787b3f60ca9547c38080e5e93c752a1e
Colin <colincmcc@gmail.com>
2018-10-03 18:09:34 -0400

    added command line args

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 const { TransactionProcessor } = require('sawtooth-sdk/processor')
+var program = require('commander');
 const HamletHandler = require('./handler')
-const address = 'tcp://127.0.0.1:4004'
+
+program
+  .version('0.1.0')
+  .option('-h, --host [value]', 'Set validator host')
+  .parse(process.argv);
+
+
+const address = program.host
+console.log(address)
 
 const transactionProcessor = new TransactionProcessor(address)
 
